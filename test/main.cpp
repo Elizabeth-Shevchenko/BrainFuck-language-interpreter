5fc29f3d36244a0880cd9101d9e31fde57bbb336
Elizabeth Shevchenko <elizabeth.shevchenko.2017@gmail.com>
2017-08-23 15:43:50 +0300

    Added possibility to build Interpreter lib from the test project, added first unit test

--- test/main.cpp
+++ test/main.cpp
@@ -1,4 +1,13 @@
 #include "gtest/gtest.h"
+#include "../include/BrainFInterpreter.h"
+
+TEST (BrainFInterpreter, RequireThat_StartInterpretation_NotThrowException_WhenCommandInvalid)
+{
+    BrainFInterpreter interpreter;
+    std::string failedTestStr = "[---]++>]----";
+
+    ASSERT_NO_THROW(interpreter.StartInterpeation(failedTestStr));
+}
 
 int main(int argc, char **argv)
 {
